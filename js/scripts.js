9f8ef70b6ad19964c69b59beee093f0aa27bc020
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-11-03 16:27:32 -0700

    add required functionality, copy paste button and logic allowing for selective input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 //business logic
 
-////LeadObjectMaker ////
-function Lead (leadName, organizationName, employeeNumber, roleType, industry, timeline, engagement, score) {
-  this.leadName=leadName;
-  this.organizationName=organizationName;
-  this.employeeNumber=employeeNumber;
-  this.roleType= roleType;
-  this.industry= industry;
-  this.timeline= timeline;
-  this.engagement= engagement;
-  this.score=score;
+function utmCodeMaker(url, source, medium, name, term, content) {
+  alert(url + source + medium + name + term + content);
+  var params = [];
+  var code = url + "?utm_source=" + source;
+
+  if (medium){
+    params.push("utm_medium=" + medium);
+  }
+  if (name){
+    params.push("utm_name=" +name);
+  }
+  if (term){
+    params.push("utm_term=" +term);
+  }
+  if (content){
+    params.push("utm_content=" +content);
+  }
+  for (var i = 0; i < params.length; i++) {
+    code = code + "&" + params[i];
+    alert(code);
+  }
+
+  return code;
+}
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
 }
 
 // NOTE: ADD YOUR DATABASE/CRM API SAVING METHOD HERE //
@@ -23,19 +44,6 @@ $(document).ready(function() {
   $("form#order-form").submit(function(event) {
     event.preventDefault();
 
-    Campaign Source
-    Campaign Medium
-    Marketing medium: (e.g. cpc, banner, email)
-    Campaign Name
-
-    34
-    Product, promo code, or slogan (e.g. spring_sale)
-    Campaign Term
-
-    4
-    Identify the paid keywords
-    Campaign Content
-
     ///Input///
     var webPage=$("#web-page").val();
     var campaignSource=$("#campaign-source").val();
@@ -43,61 +51,10 @@ $(document).ready(function() {
     var campaignName=$("#campaign-name").val();
     var campaignTerm=$("#campaign-term").val();
     var campaignContent=$("#campaign-content").val();
-    var referrer=$("#lead-role").find('option:selected').attr("name");
-    var industry=$("#industry").find('option:selected').attr("name");
-    var companySize=$("#company-size").find('option:selected').attr("name");
-    var timeline=$("#timeline").find('option:selected').attr("name");
-    var emailOpen=0;
-    if ($("#emailOpen").is(':checked')){
-      emailOpen=$('#emailOpen').val();
-    }
-    var websiteVisit=0;
-    if ($("#websiteVisit").is(':checked')){
-      websiteVisit=$('#websiteVisit').val();
-    }
-    var blogPostRead=0;
-    if ($("#blogPostRead").is(':checked')){
-      blogPostRead=$('#blogPostRead').val();
-    }
-    var whitePaperDownload=0;
-    if ($("#whitePaperDownload").is(':checked')){
-      whitePaperDownload=$('#whitePaperDownload').val();
-    }
-    var positiveResponse=0;
-    if ($("#positiveResponse").is(':checked')){
-      positiveResponse=$('#positiveResponse').val();
-    }
-    var answeredCall=0;
-    if ($("#answeredCall").is(':checked')){
-      answeredCall=$('#answeredCall').val();
-    }
-    var leadReachedOutByEmailPhone=0
-    if ($("#leadReachedOutByEmailPhone").is(':checked')){
-      leadReachedOutByEmailPhone=$('#leadReachedOutByEmailPhone').val();
-    }
-
-    // emailOpen websiteVisit blogPostRead whitePaperDownload positiveResponse answeredCall leadReachedOutByEmailPhone
-
-    var engagement=  parseInt(emailOpen) + parseInt(websiteVisit) + parseInt(blogPostRead) + parseInt(whitePaperDownload) + parseInt(positiveResponse) + parseInt(answeredCall) + parseInt(leadReachedOutByEmailPhone) + parseInt($("#engagement").val());
-
-    var score = parseInt($("#lead-role").val()) + parseInt($("#industry").val()) + parseInt($("#company-size").val()) + parseInt($("#timeline").val()) + engagement;
-
-    var newLead = new Lead(contactName, orgName, companySize, leadRole, industry, timeline, engagement, score);
-
-    // NOTE: IMPLEMENT YOUR DATABASE SAVING METHOD HERE FOR THE newLead ...
-
-
-    //
+    var utmCode=utmCodeMaker(webPage, campaignSource, campaignMedium, campaignName, campaignTerm, campaignContent);
 
     // Output
-    $(".name-output").text(contactName);
-    $(".company-output").text(orgName);
-    $(".role-output").text(leadRole);
-    $(".vertical-output").text(industry);
-    $(".size-output").text(companySize);
-    $(".timeline-output").text(timeline);
-    $(".engagement-output").text(engagement);
-    $(".score-output").text(score);
+    $("#output").text(utmCode);
 
 
   });
